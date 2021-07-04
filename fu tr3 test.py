0c007828bd08f4e494ff6fdbc0662a541ad01791
Joseph-Rance <josephhenryrance@gmail.com>
2021-07-04 17:55:03 +0100

    redo TR3

--- fu tr3 test.py
+++ fu tr3 test.py
@@ -4,12 +4,14 @@ class node:
     def __init__(self, op, *vals):
         self.op, self.vals = op, vals
 
-    def __eq__(self, graph):
+    def __eq__(self, graph):  # does not handle commutativity
         if type(graph) == str:
+            if graph == "_":
+                return True
             return False
         if len(graph.vals) != len(self.vals):
             return False
-        return graph.op == self.op and all([a == b for a, b in zip(self.vals, graph.vals)])
+        return graph.op == self.op and all(["_" in [str(a), str(b)] or a == b for a, b in zip(self.vals, graph.vals)])
 
 def TR3(graph):  # e.g. sin(-x) -> -sin(x)
 
@@ -19,39 +21,40 @@ def TR3(graph):  # e.g. sin(-x) -> -sin(x)
     if graph.op in ["sin", "cos", "tan", "cot"]:
 
         if type(graph.vals[0]) != str:
-            if graph.vals[0].op == "-" and len(graph.vals[0].vals) == 1:  # -x
-                if graph.op == "cos": return node("cos", graph.vals[0].vals[0])  # cos(-x) = cos(x)
-                return node("-", node(graph.op, graph.vals[0].vals[0]))  # sin/tan/cot(-x) = -sin/tan/cot(x)
-            if len(graph.vals[0].vals) == 2 and graph.vals[0].vals[0] == "pi" and graph.vals[0].op == "-":  # pi - x
-                if graph.op == "sin": return node("sin", graph.vals[0].vals[1])  # sin(pi - x) = sin(x)
-                return node("-", node(graph.op, graph.vals[0].vals[1]))  # cos/tan/cot(pi - x) = -cos/tan/cot(pi - x)
-            if len(graph.vals[0].vals) == 2 and \
-                        graph.vals[0].op == "+" and \
-                        "pi" in graph.vals[0].vals:  # pi + x
+
+            v = graph.vals[0]
+
+            if v == node("-", "_"):  # -x
+                if graph.op == "cos": return node("cos", v.vals[0])  # cos(-x) = cos(x)
+                return node("-", node(graph.op, v.vals[0]))  # sin/tan/cot(-x) = -sin/tan/cot(x)
+            if v == node("-", "pi", "_"):  # pi - x
+                if graph.op == "sin": return node("sin", v.vals[1])  # sin(pi - x) = sin(x)
+                return node("-", node(graph.op, v.vals[1]))  # cos/tan/cot(pi - x) = -cos/tan/cot(pi - x)
+            if v in [node("+", "pi", "_"), node("+", "_", "pi")]:  # pi + x
                 idx = 0
-                if graph.vals[0].vals[0] == "pi":  # dealing with commutativity
+                if v.vals[0] == "pi":  # dealing with commutativity
                     idx = 1
-                if graph.op in ["tan", "cot"]: return node(graph.op, graph.vals[0].vals[idx])  # tan/cot(pi + x) = tan/cot(x)
-                return node("-", node(graph.op, graph.vals[0].vals[idx]))  # sin/cos(pi + x) = -sin/cos(x)
-            if len(graph.vals[0].vals) == 2 and \
-                        (node("*", "2", "pi") in graph.vals[0].vals or node("*", "pi", "2") in graph.vals[0].vals) and \
-                        graph.vals[0].op == "-":  # 2pi - x (is this not redundant?)
-                idx = 0
-                if node("*", "2", "pi") in graph.vals[0].vals[0] or node("*", "pi", "2") in graph.vals[0].vals[0]:  # commutativity
-                    idx = 1
-                if graph.op == "cos": return node("cos", graph.vals[0].vals[idx])  # cos(2pi - x) = cos(x)
-                return node("-", node(graph.op, graph.vals[0].vals[idx]))  # sin/tan/cot(2pi - x) = -sin/tan/cot(x)
-            if len(graph.vals[0].vals) == 2 and \
-                        (any([
-                            (type(val) != str and val.op == "*" and "pi" in val.vals and eval("*".join(val.vals) + "/pi")%2 == 0)  # check if val is 2kpi ("AND" is not commutative bc eval order)
-                            for val in graph.vals[0].vals])) and \
-                        graph.vals[0].op == "+":  # 2kpi + x
-                idx = 0
-                if type(graph.vals[0].vals[0]) != str and graph.vals[0].vals[0].op == "*" and "pi" in graph.vals[0].vals[0].vals and eval("*".join(graph.vals[0].vals[0].vals) + "/pi")%2 == 0:  # commutativity
-                    idx = 1
-                return node(graph.op, graph.vals[0].vals[idx])  # sin/cos/tan/cot(2kpi + x) = sin/cos/tan/cot(x)
+                if graph.op in ["tan", "cot"]: return node(graph.op, v.vals[idx])  # tan/cot(pi + x) = tan/cot(x)
+                return node("-", node(graph.op, v.vals[idx]))  # sin/cos(pi + x) = -sin/cos(x)
+            if v in [node("-", node("*", "2", "pi"), "_"), node("-", node("*", "pi", "2"), "_")]:  # 2pi - x (is this not redundant?)
+                if graph.op == "cos": return node("cos", v.vals[1])  # cos(2pi - x) = cos(x)
+                return node("-", node(graph.op, v.vals[1]))  # sin/tan/cot(2pi - x) = -sin/tan/cot(x)
+            if v in [node("+", node("*", "_", "pi"), "_"), node("+", node("*", "pi", "_"), "_")] and \
+                    type(v.vals[0].vals[0]) == str == type(v.vals[0].vals[1]) and \
+                    eval("*".join(v.vals[0].vals)+"/pi")%2 == 0:  # 2kpi + x (but not x + 2kpi)
+                idx = 1
+                if v == node("+", node("*", "pi", "_"), "_") and v.vals[0].vals[0] == "pi":
+                    idx = 0
+                return node(graph.op, v.vals[idx])  # sin/cos/tan/cot(2kpi + x) = sin/cos/tan/cot(x)
+            if v in [node("+", "_", node("*", "_", "pi")), node("+", "_", node("*", "pi", "_"))] and \
+                    type(v.vals[1].vals[0]) == str == type(v.vals[1].vals[1]) and \
+                    eval("*".join(v.vals[1].vals)+"/pi")%2 == 0:  # x + 2kpi
+                idx = 1
+                if v == node("+", "_", node("*", "pi", "_")) and v.vals[1].vals[0] == "pi":
+                    idx = 0
+                return node(graph.op, v.vals[idx])  # sin/cos/tan/cot(x + 2kpi) = sin/cos/tan/cot(x)
                 
-    return graph(graph.op, *[TR3(i) for i in graph.vals])
+    return node(graph.op, *[TR3(i) for i in graph.vals])
 
 def get_infix(graph, parens=False):  # for printing the graph with normal notation
     if type(graph) == str:
@@ -65,7 +68,6 @@ def get_infix(graph, parens=False):  # for printing the graph with normal notati
             return "(" + get_infix(graph.vals[0], True) + ") " + graph.op + " (" + get_infix(graph.vals[1], True) + ")"
         return get_infix(graph.vals[0]) + " " + graph.op + " " + get_infix(graph.vals[1])
 
-# FIRST TRY LETS GOOO
 print(get_infix(node("sin", node("-", "x"))), "=", get_infix(TR3(node("sin", node("-", "x")))))  # -sin(x)
 print(get_infix(node("sin", node("-", "pi", "x"))), "=", get_infix(TR3(node("sin", node("-", "pi", "x")))))  # sin(x)
 print(get_infix(node("sin", node("+", node("*", "2", "pi"), "x"))), "=",
