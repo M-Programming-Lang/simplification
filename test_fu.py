bc1d935d350e1c5de9aef2642fee818d2ce479b5
Joseph-Rance <josephhenryrance@gmail.com>
2021-07-07 22:07:20 +0100

    fixed tests

--- test_fu.py
+++ test_fu.py
@@ -65,8 +65,8 @@ class TestFu(unittest.TestCase):
                         node("tan", "z")), node("-", "1", node("*", node("tan", "y"), node("tan", "z"))))))))  # apply twice for tan(x+y+z)
 
       def test_TR13(self):
-            self.assertEqual(TR13(node("*", node("tan", "x"), node("tan", "y"))), node("-", "1", node("+", node("/", node("tan", "x"), node("tan", node("+", "x", "y")), node("/", node("tan", "y"), node("tan", node("+", "x", "y")))))
-            self.assertEqual(TR13(node("sin", node("*", node("cot", "x"), node("cot", "y")))), node("sin", node("+", "1", node("+", node("/", node("cot", "x"), node("tan", node("+", "x", "y")), node("/", node("cot", "y"), node("tan", node("+", "x", "y"))))))
+            self.assertEqual(TR13(node("*", node("tan", "x"), node("tan", "y"))), node("-", "1", node("+", node("/", node("tan", "x"), node("tan", node("+", "x", "y"))), node("/", node("tan", "y"), node("tan", node("+", "x", "y"))))))
+            self.assertEqual(TR13(node("sin", node("*", node("cot", "x"), node("cot", "y")))), node("sin", node("+", "1", node("+", node("/", node("cot", "x"), node("tan", node("+", "x", "y"))), node("/", node("cot", "y"), node("tan", node("+", "x", "y")))))))
 
       def test_get_rpn(self):
             self.assertEqual(get_rpn(self.test_expr), "1 2 x * sin 2 ^ 4 / y sin 2 ^ x cos 4 ^ + + -")
